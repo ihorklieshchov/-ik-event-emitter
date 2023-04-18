2fac8680f3e37fcbd6fd6aa08a3248aad54e9afb
Ihor Klieshchov <ihorklieshchov@Ihors-MBP-2.fritz.box>
2023-04-18 14:04:19 +0200

    initial

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import typescript from '@rollup/plugin-typescript';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { visualizer } from "rollup-plugin-visualizer";
+
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/main.ts'),
+      // the proper extensions will be added
+      fileName: 'main',
+      formats: ["es", "cjs"]
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        dir: 'lib',
+        globals: {},
+      },
+      plugins: [
+        typescript({
+          sourceMap: false,
+          declaration: true,
+          outDir: "lib",
+          exclude: ["**/__tests__"],
+        }),
+        visualizer(),
+      ]
+    },
+  },
+})
